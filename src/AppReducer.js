4367b4b817e4a59e0efe8d215edc015cf771942e
Pavelkv94 <pavel.kv94@yandex.by>
2022-06-14 09:39:59 +0300

    fix

--- src/AppReducer.js
+++ src/AppReducer.js
@@ -96,10 +96,7 @@ export const BalancerStore = (state = initialState, action) => {
         case `${ActionTypes.WEB_ROUTES_GATEWAYS_FETCH}_PENDING`:
             return state.set('traefikGatewaysStatus', 'pending');
         case `${ActionTypes.WEB_ROUTES_GATEWAYS_FETCH}_REJECTED`:
-            return Immutable.merge(state, {
-                traefikGateways: [],
-                traefikGatewaysStatus: 'rejected'
-            });
+            return state.set('traefikGatewaysStatus', 'rejected');
         case `${ActionTypes.WEB_ROUTES_GATEWAYS_FETCH}_FULFILLED`:
             return Immutable.merge(state, {
                 traefikGateways: action.payload,
