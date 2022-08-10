0f99d85b31b819802e0d193b7465902c0efd1d74
Pavelkv94 <pavel.kv94@yandex.by>
2022-08-10 15:34:18 +0300

    fix service link

--- src/components/WebRoutesDetails.js
+++ src/components/WebRoutesDetails.js
@@ -37,6 +37,8 @@ const WebRoutesDetails = ({ t, history }) => {
 
     const protocol = (route?.insecure === 'None' && route?.tls_termination === '') ? 'http://' : 'https://';
 
+    const computeLink = user.location == 'dby' ? `https://compute-dev.zby.icdc.io` : `https://compute.${user.location}.icdc.io`;
+
     if (traefikRouteDeleteStatus === 'fulfilled') {
         return <Redirect to={webRoutesPath(menuGroup)} />;
     }
@@ -77,16 +79,13 @@ const WebRoutesDetails = ({ t, history }) => {
                 <Grid.Row className='web-routes-details-row-style'>
                     <Grid.Column width={4}>{t('service')}</Grid.Column>
                     <Grid.Column as='section' width={8}>
-                        {route.services.length > 0 ? route.services.map((el, i) => {
-                                
-                                const computeLink = user.location == 'dby' ? `https://compute-dev.zby.icdc.io` : `https://compute.${user.location}.icdc.io`;
-
-                                return <a 
+                        {route.services.length > 0 ? route.services.map((el, i) => 
+                                <a 
                                     key={i} 
                                     href={`${computeLink}/ui/service/services/${el.ext_id}`}
                                     target='_blank'>
                                         {`${el.name} (${el.ext_id})${i != route.services.length-1 ? ',' : ''}`}
-                                </a>}) 
+                                </a>) 
                             : t('none')}
                     </Grid.Column>
                 </Grid.Row>
