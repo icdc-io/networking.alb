c88a0a4735a56ff6dcccae1a42aafb1ca2909223
DValashchuk@ibagroup.eu <DValashchuk@ibagroup.eu>
2023-11-20 11:33:53 +0300

    Update headers part in web route form

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Form, Input } from 'semantic-ui-react';
 import { PropTypes } from 'prop-types';
 
-const FormField = ({ label, value, callback, placeholder, error }) => {
+const FormField = ({ label, value, callback, placeholder, error, ...props }) => {
     return (
         <Form.Field style={{ marginBottom: '2px' }} error={error}>
             <label>{label}</label>
-            <Input value={value} onChange={callback} placeholder={placeholder} type='text'/>
+            <Input {...props} value={value} onChange={callback} placeholder={placeholder} type='text'/>
         </Form.Field>
     );
 };
