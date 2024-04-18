52152ab01c115c19851adc5d9290ac2ea91bb091
DValashchuk@ibagroup.eu <DValashchuk@ibagroup.eu>
2024-04-18 13:58:45 +0300

    Remove logs

--- src/utilities/search.js
+++ src/utilities/search.js
@@ -11,8 +11,6 @@ const searchInDataStructure = (key, value, searchString) => {
 };
 
 export const onSearch = (array, searchString) => {
-    console.log(array)
-    console.log(searchString)
     if (!searchString) return array;
     return array.filter((item) => Object.keys(item).some(key => (
         item[key] && typeof item[key] !== 'boolean' && !setExceptions(key) && typeof item[key] !== 'number'
